84df7decca3253d1d89da2d5b47799960eaf9752
Makhsotova Milana <milana.makhsotova11@gmail.com>
2024-07-18 15:52:53 +0300

    custom table view cell

---
+++ MyPlaces/MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Милана Махсотова on 18.07.2024.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+}
